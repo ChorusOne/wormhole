9842e373e0a3c617fabfb95950f8fa55d6ef6067
Joe Bowman <joe@chorus.one>
2020-06-04 16:53:27 +0100

    add update client logic

--- pallets/tendermint-client/src/lib.rs
+++ pallets/tendermint-client/src/lib.rs
@@ -9,13 +9,12 @@ use tendermint::{
 		signed_header::SignedHeader,
 	},
 	time::Time,
-	//lite::TrustThresholdFraction,
-	validator::Set,
 };
 use sha2::{Sha256, Digest};
 #[macro_use]
 extern crate alloc;
 use  sp_std::vec::Vec;
+use log::{error, debug};
 
 mod types;
 
@@ -87,61 +86,85 @@ decl_module! {
 		fn deposit_event() = default;
 
 		/// Client initialisation entry point.
-		/// function that can be called by the external world as an extrinsics call
-		/// takes Tendermint::SignedHeader, trust_period as Duration and a client_name as String, validates the SignedHeader, creates a TendermintClient and stores it, emitting an event.
+		/// takes tendermint/ibc/tendermint/CreateClient message.
 		#[weight = 100_000]
 		pub fn init_client(origin, payload: Vec<u8>) -> dispatch::DispatchResult {
+			debug!("init client");
+			debug!("Submitted payload: {:?}", &payload[..]);
 			// Check it was signed and get the signer. See also: ensure_root and ensure_none
 			let who = ensure_signed(origin)?;
-
-			let container: TMCreateClientPayload = serde_json::from_slice(&payload[..]).map_err(|_e| Error::<T>::DeserializeError)?;
-
-			let header: SignedHeader = container.header;
-			let trust_period: u64 = container.trust_period;
-			// validate header
-			// validate client name
-			// validate trust_period
+			let r: Result<_, _> = serde_json::from_slice(&payload[..]);
+			let container: TMCreateClientPayload = r.map_err(|e| {
+			  error!("Deserialization Error: {}", e);
+			  Error::<T>::DeserializeError
+			})?;
+			let header: SignedHeader = container.header.signed_header;
+			let trust_period: u64 = container.trusting_period;
+			let max_clock_drift: u64 = container.max_clock_drift;
+			let unbonding_period: u64 = container.unbonding_period;
+			// TODO:  validate header
+			// TODO:  validate client name
+			// TODO:  validate trust_period
 			let state: ConsensusState = ConsensusState{
 				signed_header: header.clone(),
 				height: header.header.height.value(),
 				last_update: Time::now(),
-				next_validator_set: Set::new(vec![]) // TODO: populate this!
+				next_validator_set: container.validator_set
 			};
 
 			let tmclient: TendermintClient = TendermintClient{
 				state: Some(state.clone()),
 				trusting_period: trust_period,
-				client_name: container.client_name.clone(),
+				client_id: container.client_id.clone(),
+				max_clock_drift: max_clock_drift,
+				unbonding_period: unbonding_period,
 				chain_id: header.header.chain_id.as_bytes().to_vec(),
 			};
 
 			let mut hasher = Sha256::new();
-			hasher.input(&container.client_name);
+			hasher.input(&container.client_id);
 			let key = hasher.result();
+
 			TMClientStorage::insert(key.as_slice(), TMClientStorageWrapper{client: tmclient.clone()});
+			// TODO: does this error if the key already exists?
 
 			// Here we are raising the ClientCreated event
-			Self::deposit_event(RawEvent::ClientCreated(who, tmclient.client_name, tmclient.chain_id, state.height));
+			Self::deposit_event(RawEvent::ClientCreated(who, tmclient.client_id, tmclient.chain_id, state.height));
 			Ok(())
 		}
 
-		/// Another dummy entry point.
-		/// takes no parameters, attempts to increment storage value, and possibly throws an error
+		/// Update client entry point.
 		#[weight = 100_000]
 		pub fn update_client(origin, payload: Vec<u8>) -> dispatch::DispatchResult {
 
-			let container: TMUpdateClientPayload = serde_json::from_slice(&payload[..]).map_err(|_e| Error::<T>::DeserializeError)?;
-
 			// Check it was signed and get the signer. See also: ensure_root and ensure_none
 			let _who = ensure_signed(origin)?;
+
+			let r: Result<_, _> = serde_json::from_slice(&payload[..]);
+			let container: TMUpdateClientPayload = r.map_err(|e| {
+			  error!("Deserialization Error: {}", e);
+			  Error::<T>::DeserializeError
+			})?;
+
 			let mut hasher = Sha256::new();
-			hasher.input(&container.client_name);
+			hasher.input(&container.client_id);
 			let key = hasher.result();
+
 			ensure!(TMClientStorage::contains_key(key.as_slice()), Error::<T>::ItemNotFound);
 
-            // Get owner of the claim
-            let _wrapped_client: TMClientStorageWrapper = TMClientStorage::get(key.as_slice());
+            let mut wrapped_client: TMClientStorageWrapper = TMClientStorage::get(key.as_slice());
 
+			let header: SignedHeader = container.header.signed_header;
+
+			// TODO:  validate header
+			let state: ConsensusState = ConsensusState{
+				signed_header: header.clone(),
+				height: header.header.height.value(),
+				last_update: Time::now(),
+				next_validator_set: wrapped_client.client.state.unwrap().next_validator_set // TODO: handle validator set changes?
+			};
+			wrapped_client.client.state = Some(state.clone());
+			TMClientStorage::insert(key.as_slice(), wrapped_client.clone());
 			Ok(())
 		}
 	}
--- pallets/tendermint-client/src/types.rs
+++ pallets/tendermint-client/src/types.rs
@@ -12,16 +12,24 @@ use tendermint::{
 };
 
 #[derive(Serialize, Deserialize, Clone, Debug)]
+pub struct TMHeader {
+    pub signed_header: SignedHeader,
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug)]
 pub struct TMCreateClientPayload {
-    pub header: SignedHeader,
-    pub trust_period: u64,
-    pub client_name: Vec<u8>,
+    pub header: TMHeader,
+    pub validator_set: ValidatorSet,
+    pub trusting_period: u64,
+    pub max_clock_drift: u64,
+    pub unbonding_period: u64,
+    pub client_id: Vec<u8>,
 }
 
 #[derive(Serialize, Deserialize, Clone, Debug)]
 pub struct TMUpdateClientPayload {
-    pub header: SignedHeader,
-    pub client_name: Vec<u8>,
+    pub header: TMHeader,
+    pub client_id: Vec<u8>,
 }
 
 #[derive(Serialize, Deserialize, Clone, Debug)]
@@ -35,18 +43,25 @@ pub struct ConsensusState {
 #[derive(Serialize, Deserialize, Clone, Debug)]
 pub struct TendermintClient {
     pub state: Option<ConsensusState>,
-    pub client_name: Vec<u8>,
+    pub client_id: Vec<u8>,
     pub chain_id: Vec<u8>,
     pub trusting_period: u64,
+    pub max_clock_drift: u64,
+    pub unbonding_period: u64,
+    //pub owner: cosmosAddress,
 }
+
+
 
 impl Default for TendermintClient {
     fn default() -> Self {
         TendermintClient {
             state: None,
-            client_name: Vec::new(),
+            client_id: Vec::new(),
             chain_id: Vec::new(),
             trusting_period: 86400,
+            max_clock_drift: 30,
+            unbonding_period: 86400*7*3
         }
     }
 }
